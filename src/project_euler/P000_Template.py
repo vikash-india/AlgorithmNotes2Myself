496d9cb62fc80c5ea3ec8d7f77e4af29b0e35df4
Vikash <vikash.kmc@gmail.com>
2015-07-02 18:12:54 +0530

    Add template files for Project Euler programming problems.

---
+++ src/project_euler/P000_Template.py
@@ -0,0 +1,27 @@
+# Description: Write Here
+
+"""
+Technique
+- Write Here
+
+Note
+- Write Here
+
+Instrumentation
+- System Details: 8x Intel Core i7-3630QM CPU @ 2.40GHz, 16GB RAM, Ubuntu 14.04
+- Input Details: Write Here
+- Time for 100 runs: Minimum - NA sec, Average - NA sec, Maximum NA sec
+- Write Here
+"""
+def function_name():
+    pass
+
+# Main
+def main():
+    """Main function to test the above implementation. """
+
+    result = function_name()
+    print 'Write Here {0} '.format(result)
+
+# Call Main
+main()
---
+++ src/project_euler/P000_Template_Idea.md
@@ -0,0 +1,12 @@
+# Write Here
+
+### Problem Statement
+- Write Here
+- [Project Euler URL](http://projecteuler.net/problem=1)
+- Solution: Write Here
+
+### Techniques
+1. Write Here
+
+### Note
+- None
