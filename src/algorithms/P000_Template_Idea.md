8ab1ec6c4807d2d0da9695e8a366a6a63160d6d7
Vikash <vikash.kmc@gmail.com>
2015-07-11 02:12:01 +0530

    Add template file for this directory.

---
+++ src/algorithms/P000_Template_Idea.md
@@ -0,0 +1,19 @@
+# Description: WriteHere
+
+### Sub Title Here
+- Write here
+
+### Sub Title Here
+- Write here
+
+### Algorithm
+1. Write here
+
+### Optimisation(s)
+- None
+
+### Complexity
+- NA
+
+### TODO
+- None
---
+++ src/algorithms/P000_Template_Solution_1.py
@@ -0,0 +1,32 @@
+# Description: Write Here
+
+import logging
+
+"""
+Note
+1. Write Here
+2. Optimisations
+    - Write Here
+3. Limitations
+    - Write Here
+"""
+
+def function_name(number):
+    """Write Here"""
+
+    logging.debug("Execution started")
+
+    pass
+
+def main():
+    """Main function to test the above implementation. """
+
+    # Set logging level from DEBUG, INFO, WARNING. ERROR, CRITICAL
+    logging.basicConfig(level=logging.DEBUG)
+
+    number = 0
+    result = function_name(number)
+    print '{0} gives the result {1}.'.format(number, result)
+
+# Call Main
+main()
