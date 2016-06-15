5eccc3b33bfef9b1d808692ad637691d5901bbfe
Vikash <vikash.kmc@gmail.com>
2016-06-16 02:18:35 +0530

    Modify Testcase file format.

--- src/000_template/solution_01_tests.py
+++ src/000_template/solution_01_tests.py
@@ -1,11 +1,14 @@
-from src.google_foobar.P000_template.solution_01 import answer
 import unittest
+
+from src.google_foobar.P000_template.solution_01 import answer
+
 
 class TestSolution(unittest.TestCase):
     def testcase_001(self):
-        names = []
-        expected = []
-        self.assertEqual(answer(names), expected)
+        names = None
+        expected = None
+        self.assertEqual(answer(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
