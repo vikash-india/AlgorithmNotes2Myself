5ce5a0e4b6c1aafd31d1f22c3c0b2f9d2070ba77
Vikash <vikash.kmc@gmail.com>
2016-07-20 03:08:02 +0530

    Understand visualise all small cases upto N = 4.

--- src/000_template/solution_01_tests.py
+++ src/000_template/solution_01_tests.py
@@ -5,9 +5,9 @@ from src.google_foobar.P000_template.solution_01 import answer
 
 class TestSolution(unittest.TestCase):
     def testcase_001(self):
-        names = None
         expected = None
-        self.assertEqual(answer(), expected)
+        names = None
+        self.assertEqual(expected, answer())
 
 
 if __name__ == '__main__':
